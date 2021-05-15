6d622a08788571026af5ee64a2df07ecf80a8712
Hossein <djalicrt@gmail.com>
2021-05-15 17:42:55 +0430

    impl. Add List with Input and BackGround Color is done!

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native';
 import {AntDesign} from '@expo/vector-icons'
 import colors from './Colors'
 import tempData from './tempData'
 import TodoList from './components/TodoList'
+import AddListModal from './components/AddListModal'
 
 export default class App extends React.Component {
-  render() {
+  
+    state = {
+      addTodoVisible: true
+    };
 
+    toggleAddTodoModal() {
+      this.setState ({ addTodoVisible: !this.state.addTodoVisible});
+    }
+
+    render() {
     return (
       <View style={styles.container}>
+        <Modal animationType="slide"
+        visible={this.state.addTodoVisible}
+        onRequestClose = {() => this.toggleAddTodoModal()}
+        >
+        <AddListModal closeModal={() => this.toggleAddTodoModal()} />
+        </Modal>
+
+
         <View style={{ flexDirection: "row" }}>
           <View style={styles.divider} />
           <Text style={styles.title} >
@@ -18,7 +35,7 @@ export default class App extends React.Component {
           <View style={styles.divider} />
         </View>
         <View style= {{ marginVertical: 48}}>
-          <TouchableOpacity style={styles.addList}>
+          <TouchableOpacity style={styles.addList} onPress= {() => this.toggleAddTodoModal()}>
               <AntDesign name="plus" size={16} color={colors.blue} />
           </TouchableOpacity>
 
---
+++ components/AddListModal.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { View , Text , StyleSheet, KeyboardAvoidingView, TouchableOpacity, TextInput } from "react-native";
+import { AntDesign } from '@expo/vector-icons'
+import colors from '../Colors'
+
+export default class AddListModal extends React.Component {
+
+    backgroundColors = ["#f44336" , "#1e88e5", "#43a047" , "#ffa000" , "#7b1fa2" , "#90a4ae"];
+    state = {
+        name: "",
+        color: this.backgroundColors[0]
+    }
+
+    renderColors() {
+        return this.backgroundColors.map(color => {
+            return (
+                <TouchableOpacity
+                key={color}
+                style={[styles.colorSelect, {backgroundColor: color}]}
+                onPress={() => this.setState({color})}
+                />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView style={styles.container} behavior="padding">
+                <TouchableOpacity style={{ position: "absolute", top: 40, right: 32 }} onPress={this.props.closeModal}>
+                    <AntDesign name="close" size={24} color={colors.black}></AntDesign>
+                </TouchableOpacity>
+            <View style={{alignSelf: "stretch", marginHorizontal: 32}}>
+                <Text style={styles.title}>Create Todo List</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="List Name"
+                        onChangeText={text => this.setState({name: text})}></TextInput>
+
+                <View style={{ flexDirection: "row" , justifyContent: "space-between", marginTop: 12}}>
+                    {this.renderColors()}
+                </View>
+
+                <TouchableOpacity style={[styles.create , {backgroundColor: this.state.color}]}>
+                    <Text style={{ color: colors.white, fontWeight: "600"}}>Create!</Text>
+                </TouchableOpacity>
+
+
+            </View>
+
+            </KeyboardAvoidingView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: "800",
+        color: colors.black,
+        alignSelf: "center",
+        marginBottom: 16
+    },
+    input: {
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: colors.blue,
+        borderRadius: 6,
+        height: 50,
+        marginTop: 8,
+        paddingHorizontal: 16,
+        fontSize: 18
+    },
+    create: {
+        marginTop: 24,
+        height: 50,
+        borderRadius: 6,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    colorSelect: {
+        width: 30,
+        height: 30,
+
+    }
+
+})
